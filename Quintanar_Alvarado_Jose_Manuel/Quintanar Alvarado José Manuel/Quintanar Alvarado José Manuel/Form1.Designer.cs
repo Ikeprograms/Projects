f46e6e9ffdc7eb245041575a3e31290797c58bc3
Ikeprograms <59674379+Ikeprograms@users.noreply.github.com>
2020-10-07 20:20:19 -0700

    Add files via upload

---
+++ Quintanar_Alvarado_Jose_Manuel/Quintanar Alvarado José Manuel/Quintanar Alvarado José Manuel/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace Quintanar_Alvarado_José_Manuel
+{
+    partial class Ventana
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Ventana));
+            this.btnSaludo = new System.Windows.Forms.Button();
+            this.btnRojo = new System.Windows.Forms.Button();
+            this.btnAzul = new System.Windows.Forms.Button();
+            this.btnVerde = new System.Windows.Forms.Button();
+            this.btnAmarillo = new System.Windows.Forms.Button();
+            this.btnMorado = new System.Windows.Forms.Button();
+            this.btnNaranja = new System.Windows.Forms.Button();
+            this.btnRosa = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSaludo
+            // 
+            this.btnSaludo.BackColor = System.Drawing.Color.White;
+            this.btnSaludo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSaludo.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSaludo.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSaludo.ForeColor = System.Drawing.Color.Black;
+            this.btnSaludo.Location = new System.Drawing.Point(69, 34);
+            this.btnSaludo.Name = "btnSaludo";
+            this.btnSaludo.Size = new System.Drawing.Size(120, 35);
+            this.btnSaludo.TabIndex = 0;
+            this.btnSaludo.Text = "Saludar";
+            this.btnSaludo.UseVisualStyleBackColor = false;
+            this.btnSaludo.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnRojo
+            // 
+            this.btnRojo.BackColor = System.Drawing.Color.Red;
+            this.btnRojo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnRojo.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnRojo.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRojo.Location = new System.Drawing.Point(249, 34);
+            this.btnRojo.Name = "btnRojo";
+            this.btnRojo.Size = new System.Drawing.Size(120, 35);
+            this.btnRojo.TabIndex = 1;
+            this.btnRojo.Text = "Rojo";
+            this.btnRojo.UseMnemonic = false;
+            this.btnRojo.UseVisualStyleBackColor = false;
+            this.btnRojo.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnAzul
+            // 
+            this.btnAzul.BackColor = System.Drawing.Color.Blue;
+            this.btnAzul.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAzul.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnAzul.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAzul.Location = new System.Drawing.Point(426, 34);
+            this.btnAzul.Name = "btnAzul";
+            this.btnAzul.Size = new System.Drawing.Size(120, 35);
+            this.btnAzul.TabIndex = 2;
+            this.btnAzul.Text = "Azul";
+            this.btnAzul.UseVisualStyleBackColor = false;
+            this.btnAzul.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btnVerde
+            // 
+            this.btnVerde.BackColor = System.Drawing.Color.Lime;
+            this.btnVerde.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnVerde.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnVerde.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVerde.Location = new System.Drawing.Point(69, 112);
+            this.btnVerde.Name = "btnVerde";
+            this.btnVerde.Size = new System.Drawing.Size(120, 35);
+            this.btnVerde.TabIndex = 3;
+            this.btnVerde.Text = "Verde";
+            this.btnVerde.UseVisualStyleBackColor = false;
+            this.btnVerde.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // btnAmarillo
+            // 
+            this.btnAmarillo.BackColor = System.Drawing.Color.Yellow;
+            this.btnAmarillo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAmarillo.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnAmarillo.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAmarillo.Location = new System.Drawing.Point(249, 112);
+            this.btnAmarillo.Name = "btnAmarillo";
+            this.btnAmarillo.Size = new System.Drawing.Size(120, 35);
+            this.btnAmarillo.TabIndex = 4;
+            this.btnAmarillo.Text = "Amarillo";
+            this.btnAmarillo.UseVisualStyleBackColor = false;
+            this.btnAmarillo.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // btnMorado
+            // 
+            this.btnMorado.BackColor = System.Drawing.Color.Purple;
+            this.btnMorado.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnMorado.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnMorado.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMorado.Location = new System.Drawing.Point(426, 112);
+            this.btnMorado.Name = "btnMorado";
+            this.btnMorado.Size = new System.Drawing.Size(120, 35);
+            this.btnMorado.TabIndex = 5;
+            this.btnMorado.Text = "Morado";
+            this.btnMorado.UseVisualStyleBackColor = false;
+            this.btnMorado.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // btnNaranja
+            // 
+            this.btnNaranja.BackColor = System.Drawing.Color.Orange;
+            this.btnNaranja.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnNaranja.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnNaranja.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNaranja.Location = new System.Drawing.Point(69, 187);
+            this.btnNaranja.Name = "btnNaranja";
+            this.btnNaranja.Size = new System.Drawing.Size(120, 35);
+            this.btnNaranja.TabIndex = 6;
+            this.btnNaranja.Text = "Naranja";
+            this.btnNaranja.UseVisualStyleBackColor = false;
+            this.btnNaranja.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // btnRosa
+            // 
+            this.btnRosa.BackColor = System.Drawing.Color.Pink;
+            this.btnRosa.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnRosa.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnRosa.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRosa.Location = new System.Drawing.Point(249, 187);
+            this.btnRosa.Name = "btnRosa";
+            this.btnRosa.Size = new System.Drawing.Size(120, 35);
+            this.btnRosa.TabIndex = 7;
+            this.btnRosa.Text = "Rosa";
+            this.btnRosa.UseVisualStyleBackColor = false;
+            this.btnRosa.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnSalir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSalir.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.ForeColor = System.Drawing.Color.Black;
+            this.btnSalir.Location = new System.Drawing.Point(426, 187);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(120, 35);
+            this.btnSalir.TabIndex = 8;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // Ventana
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(609, 256);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnRosa);
+            this.Controls.Add(this.btnNaranja);
+            this.Controls.Add(this.btnMorado);
+            this.Controls.Add(this.btnAmarillo);
+            this.Controls.Add(this.btnVerde);
+            this.Controls.Add(this.btnAzul);
+            this.Controls.Add(this.btnRojo);
+            this.Controls.Add(this.btnSaludo);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Ventana";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Quintanar Alvarado José Manuel";
+            this.Load += new System.EventHandler(this.Ventana_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSaludo;
+        private System.Windows.Forms.Button btnRojo;
+        private System.Windows.Forms.Button btnAzul;
+        private System.Windows.Forms.Button btnVerde;
+        private System.Windows.Forms.Button btnAmarillo;
+        private System.Windows.Forms.Button btnMorado;
+        private System.Windows.Forms.Button btnNaranja;
+        private System.Windows.Forms.Button btnRosa;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ Quintanar_Alvarado_Jose_Manuel/Quintanar Alvarado José Manuel/Quintanar Alvarado José Manuel/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quintanar_Alvarado_José_Manuel
+{
+    public partial class Ventana : Form
+    {
+        public Ventana()
+        {
+            InitializeComponent();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Amarillo", "Botón amarillo");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Rojo", "Botón rojo");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("¡Hola!", "Saludo");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Azul", "Botón azul");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Verde", "Botón verde");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Morado", "Botón morado");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Naranja", "Botón naranja");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Rosa", "Botón rosa");
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Ventana_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
