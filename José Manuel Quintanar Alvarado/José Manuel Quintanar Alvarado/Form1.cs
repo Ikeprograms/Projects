f052d00d54c34208b5580b0498ed6c05488baf32
Ikeprograms <59674379+Ikeprograms@users.noreply.github.com>
2020-10-14 17:55:21 -0700

    Add files via upload

---
+++ José Manuel Quintanar Alvarado/José Manuel Quintanar Alvarado/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace José_Manuel_Quintanar_Alvarado
+{
+    public partial class Form1 : Form
+    {
+        int contador = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (contador <= 25)
+            {
+                textBox1.Text = Convert.ToString(contador);
+                contador += 1;
+            }
+            else
+            {
+                MessageBox.Show("Límite de números: 25");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || textBox1.Text == null)
+            {
+                MessageBox.Show("La casilla está vacía");
+            }
+            else
+            {
+                textBox1.Text = "";
+            }
+        }
+    }
+}
