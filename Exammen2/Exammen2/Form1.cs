5112caf0d74c2384031ed14aafb7009bc4e8604e
Jose Quintanar <59674379+Ikeprograms@users.noreply.github.com>
2020-10-28 17:51:05 -0700

    Add files via upload

---
+++ Exammen2/Exammen2/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exammen2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Fahrenheit_Click(object sender, EventArgs e)
+        {
+            double GF;
+            double GC;
+
+            GF = double.Parse(this.textBox2.Text);
+            GC = 5.0 / 9.0 * (GF + 32);
+            this.textBox1.Text = GC.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double GF;
+            double GC;
+
+            GC = double.Parse(this.textBox2.Text);
+            GF = (GC * 9 / 5) + 32;
+            this.textBox1.Text = GF.ToString();
+        }
+    }
+}
